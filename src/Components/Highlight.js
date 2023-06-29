b1f6eb21ca73818c062a9187a161cbe9be00d889
Zeck2 <118732833+Zeck2@users.noreply.github.com>
2023-06-30 01:03:13 +0800

    Highlight Done

---
+++ src/Components/Highlight.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Highlight() {
+    return(
+        <div id='HighlightDiv'>
+            <div id='CenterBox'>
+                <div id='Elements'>
+                    <h1 className='MainTexts'>Start Your Day With Coffee!</h1>
+                    <p className='MainTexts'>
+                        Wake up and start your morning routine with US! 
+                        We have the coffees the coffees you need mouthful combined with dreamy aroma!
+                    </p>
+                    <button className='MainTexts'>BUY NOW</button>
+                </div>
+            </div>
+        </div>
+    )
+}
