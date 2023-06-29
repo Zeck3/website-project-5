1883fa016a3c507972f090deb43c83462bd3c132
Zeck2 <118732833+Zeck2@users.noreply.github.com>
2023-06-30 01:02:40 +0800

    Footer Done

---
+++ src/Components/Footer.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export default function Footer() {
+    return(
+        <div id='FooterDiv'>
+            <div className='Columns'>
+                <h3>Shop</h3>
+                <ul>Gingoog Branch</ul>
+                <ul>Cagayan De Oro Branch</ul>
+                <ul>Butuan Branch</ul>
+                <ul>Shop Online via FoodPanda</ul>
+            </div>
+            <div className='Columns'>
+                <h3>Learn</h3>
+                <ul>Brewing Techniques</ul>
+                <ul>Health Benefits</ul>
+                <ul>Caffeine Content</ul>
+                <ul>Workshops</ul>
+            </div>
+            <div className='Columns'>
+                <h3>More From Barako Café</h3>
+                <ul>FAQ's</ul>
+                <ul>Our Story</ul>
+                <ul>Blog</ul>
+                <ul>Contact Us</ul>
+                <ul>Affiliate</ul>
+            </div>
+            <div className='Columns'>
+            </div>
+            <div className='Columns'>
+                <div id='Logo'>
+                    <h1 className='LogoTexts'>BARAKO</h1>
+                    <h2 className='LogoTexts'>Café</h2>
+                </div>
+                <h3>Contacts</h3>
+                <ul>Facebook</ul>
+                <ul>Youtube</ul>
+                <ul>instagram</ul>
+            </div>
+        </div>
+    )
+}
