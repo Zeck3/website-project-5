47ba4ae5e1651a0a110222693fac54a7c256741b
Zeck2 <118732833+Zeck2@users.noreply.github.com>
2023-07-02 16:12:01 +0800

    Highlight Panels Updated

---
+++ src/Components/HighlightPanelRight.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function HighlightPanelRight() {
+    return(
+        <div id='HighlightPanelRightDiv'>
+            <div id='Container'>
+                <div className='Columns'>
+                </div>
+                <div className='Columns'>
+                </div>
+            </div>
+        </div>
+    )
+}
