23df1a8519d817a12a73184384801ef01b05f132
Zeck2 <118732833+Zeck2@users.noreply.github.com>
2023-07-31 15:34:48 +0800

    Images and svgs added in Image Assets

--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import PanelLeft from '../Components/PanelLeft';
+import PanelCenter from '../Components/PanelCenter';
 
 export default function Products() {
     return(
@@ -9,6 +11,12 @@ export default function Products() {
                 <Header />
             </header>
             <main>
+                <section>
+                    <PanelLeft />
+                </section>
+                <section>
+                    <PanelCenter />
+                </section>
             </main>
             <footer>
                 <Footer />
