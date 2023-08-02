cf55e2c4a0b151e7d44f443d0d827b95fdf20e07
Zeck2 <118732833+Zeck2@users.noreply.github.com>
2023-08-02 14:35:59 +0800

    updated app js and index js

--- src/App.js
+++ src/App.js
@@ -6,14 +6,12 @@ import Business from './Pages/Business';
 import AboutUs from './Pages/AboutUs';
 
 import './App.css';
-import './MainComponents.css';
-
 
 export default function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/products' element={<Products />}></Route>
+      <Route path='/home' element={<Home />}></Route>
+      <Route path='/' element={<Products />}></Route>
       <Route path='/business' element={<Business />}></Route>
       <Route path='/aboutus' element={<AboutUs />}></Route>
     </Routes>
