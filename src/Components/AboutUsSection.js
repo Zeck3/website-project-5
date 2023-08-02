03008e629b5e11149d9ee691a2a851a361a0be7a
Zeck2 <118732833+Zeck2@users.noreply.github.com>
2023-08-02 14:36:44 +0800

    updated about us page

---
+++ src/Components/AboutUsSection.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function AboutUsSection() {
+    return(
+        <div className="AboutUsSectionDiv">
+            <div className="Image">
+                
+            </div>
+            <div className="Container">
+
+            </div>
+        </div>
+    )
+}
